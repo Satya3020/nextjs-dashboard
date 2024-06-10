05d1f4f4682383c8764a04346a45a01b5c6b7580
satyavarssheni Ra V <satyavarsshenira.24cs@licet.ac.in>
2024-06-10 20:07:49 +0530

    Added features for customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,31 @@
 import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import Table from '@/app/ui/customers/table';
  
 export const metadata: Metadata = {
   title: 'Customers',
 };
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
 
-export default function Page(){
-    return <p>customers page</p>;
-    
+return (
+  <div className="w-full">
+    <div className="flex w-full items-center justify-between">
+      <h1 className={`${lusitana.className} text-2xl`}></h1>
+    </div>
+    <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+    </div>
+   <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+      <Table query={query} />
+    </Suspense>
+  </div>
+);
 }
